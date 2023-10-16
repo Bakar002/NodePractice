739fd7e55028e4563bd4841e62eb8ea407513548
Bakar002 <abubakaramjad78600@gmail.com>
2023-10-16 16:28:48 +0500

    os modules

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ const os =require('os')
 const user = os.userInfo()
 console.log(user)
 console.log(`the system uptime is ${os.uptime()} seconds`)
+
+
+
+const currentos={
+name:os.type(),
+release:os.release(),
+totalmemory:os.totalmem(),
+freeMemory:os.freemem(),
+
+
+
+}
+console.log(currentos)
