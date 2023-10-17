69f776c56a344e28815c08497a36c50f6a957cef
Bakar002 <abubakaramjad78600@gmail.com>
2023-10-17 22:56:36 +0500

    adding http modules

---
+++ app.js
@@ -0,0 +1,17 @@
+const http=require('http')
+const server =http.createServer((req,res)=>{
+if (req.url=='/') {
+res.end('Welcome to our first page');
+    
+}
+if (req.url=='/about') {
+    res.end('Here is our history');
+        
+    }
+    
+    res.end(`<h1>Oops!  </h1>    <p>We cant see any thing on this page </p>
+    <a href="/">Back home</a>
+    `);
+
+})
+server.listen(5000)
